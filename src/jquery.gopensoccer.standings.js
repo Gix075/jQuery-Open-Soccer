ef9602e728d80f7107269dbe87ee0d3d1362b16b
Gix075 <gildo.giuliani@gmail.com>
2016-09-25 16:01:30 +0200

    v1.1.0

--- src/jquery.gopensoccer.standings.js
+++ src/jquery.gopensoccer.standings.js
@@ -1,19 +1,8 @@
-// the semi-colon before function invocation is a safety net against concatenated
-// scripts and/or other plugins which may not be closed properly.
+
 ;( function( $, window, document, undefined ) {
 
 	"use strict";
 
-		// undefined is used here as the undefined global variable in ECMAScript 3 is
-		// mutable (ie. it can be changed by someone else). undefined isn't really being
-		// passed in so we can ensure the value of it is truly undefined. In ES5, undefined
-		// can no longer be modified.
-
-		// window and document are passed through as local variables rather than global
-		// as this (slightly) quickens the resolution process and can be more efficiently
-		// minified (especially when both are regularly referenced in your plugin).
-
-		// Create the defaults once
 		var pluginName = "gOpenSoccer_getStandings",
 			defaults = {
                 apiBaseURL: "http://soccer.sportsopendata.net/v1/leagues/",
@@ -24,14 +13,9 @@
                 debug: false
 			};
 
-		// The actual plugin constructor
+		// Plugin constructor
 		function Plugin ( element, options ) {
 			this.element = element;
-
-			// jQuery has an extend method which merges the contents of two or
-			// more objects, storing the result in the first object. The first object
-			// is generally empty as we don't want to alter the default options for
-			// future instances of the plugin
 			this.settings = $.extend( {}, defaults, options );
 			this._defaults = defaults;
 			this._name = pluginName;
@@ -41,13 +25,7 @@
 		// Avoid Plugin.prototype conflicts
 		$.extend( Plugin.prototype, {
 			init: function() {
-
-				// Place initialization logic here
-				// You already have access to the DOM element and
-				// the options via the instance, e.g. this.element
-				// and this.settings
-				// you can add more functions like the one below and
-				// call them like the example below
+                
                 var plugin = this;
                 switch(this.settings.get) {
                     case "standings":
@@ -62,7 +40,6 @@
             
 			getStandings: function(plugin,team) {
 
-				// some logic
 				$.ajax({
                     url: plugin.settings.apiBaseURL+plugin.settings.league+"/seasons/"+plugin.settings.season+"/standings",
                     dataType: "json",
